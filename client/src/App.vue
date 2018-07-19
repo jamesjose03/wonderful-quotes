c3545c90fc33724fb8f6c964443c6edc5c3245db
James Kaviyil Jose <jamesjose03@gmail.com>
2018-07-19 20:22:56 +0530

    Initial Setup

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue'
+export default{
+    data: function(){
+        return{
+            quotes:['This is a test Quote'], 
+            maxQuotes: 10
+        }
+    },
+    components:{
+        appQuoteGrid: QuoteGrid
+    }
+}
+
+</script>
+
+<style>
+</style>
---
+++ client/src/components/QuoteGrid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <app-quote v-for="quote in quotes">{{ quotes }}</app-quote>
+    </div>  
+</template>
+
+<script>
+import Quote from './Quote.vue'
+export default{
+    props: ['quotes'],
+    components:{
+        appQuote: Quote
+    }
+}
+</script>
+
+<style>
+</style>
